b0242122422a401b6e297f7c7e793be7c7e5b952
Ajay v <vajay4834@gmail.com>
2023-11-15 22:27:59 +0530

    nav bar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './NavBar.css'
-
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 const NavBar = () => {
   return (
-    <div className='navbar'>NavBar</div>
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>BuyIt</p>
+        </div>
+        <ul className='nav-menu'>
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+<button>Login</button>
+<img src={cart_icon} alt="" />
+        </div>
+    </div>
   )
 }
 
