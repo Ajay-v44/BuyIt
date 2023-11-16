8c245a3254ac002c2386c0ce26526392eafd3fd5
Ajay v <vajay4834@gmail.com>
2023-11-16 11:47:01 +0530

    routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-
+import Product from './pages/Product';
+import ShopCategory from './pages/ShopCategory';
+import { BrowserRouter, Rotes, Route } from '/react-router-dom';
 function App() {
   return (
-   <>
-    <NavBar/>
-   </>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Rotes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="men" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+        </Rotes>
+      </BrowserRouter>
+    </>
   );
 }
 
