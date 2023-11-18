1529e9f291125918d5dd2513bb882c93e4500aca
Ajay v <vajay4834@gmail.com>
2023-11-18 10:13:07 +0530

    new components

--- frontend/src/components/Hero/hero.jsx
+++ frontend/src/components/Hero/hero.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import './heo.css'
-import hand_icon from'../Assets/hand_icon.png'
-import arrow from'../Assets/arrow.png'
-import hero_image from'../Assets/hero_image.png'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 const hero = () => {
   return (
+   <>
     <div className='hero'>
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
         <div>
-          <div className="hand-hand-icon">
+          <div className="hero-hand-icon">
             <p>new</p>
             <img src={hand_icon} alt="" />
           </div>
         </div>
-      
-    <p>Collection</p>
-    <p>for everyone</p>
-    </div>
-    <div className="hero-latest-btn">
-      <div>
-        <div>Latest Collection</div>
-        <img src={arrow} alt="" />
+
+        <p>Collection</p>
+        <p>for everyone</p>
+        <div className="hero-latest-btn">
+
+<div>Latest Collection</div>
+<img src={arrow} alt="" />
+
+</div>
       </div>
-    </div>
+     
+      
       <div className="hero-right">
         <img src={hero_image} alt="" />
       </div>
     </div>
+   </>
   )
 }
 
---
+++ frontend/src/components/popular/popular.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './popular.css';
+const popular = () => {
+  return (
+    <div className='popular'>
+
+
+    </div>
+  )
+}
+
+export default popular
