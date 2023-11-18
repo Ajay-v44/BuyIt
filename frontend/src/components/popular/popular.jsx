812f9e97e4dff5d3bf58f5b373807fef1f16c8b8
Ajay v <vajay4834@gmail.com>
2023-11-18 11:11:30 +0530

    popular compoent completed

--- frontend/src/components/popular/popular.jsx
+++ frontend/src/components/popular/popular.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import data_product from '../Assets/data';
 import './popular.css';
+import Item from '../item/Item'
 const popular = () => {
-  return (
-    <div className='popular'>
+    return (
+        <div className='popular'>
+            <h1>POPULAR IN WOMEN</h1>
+            <hr />
+            <div className="popular-item">
+            {data_product.map((item) => (
+                    <Item key={item.id} id={item.id} name={item.name} image={item.image} old_price={item.old_price} new_price={item.new_price} />
+                ))}
+            </div>
+        </div>
 
 
-    </div>
-  )
+
+    )
 }
 
 export default popular
