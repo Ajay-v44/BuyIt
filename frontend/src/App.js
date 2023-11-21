f0e379611552631bb1b873150c8128861ef79eb0
Ajay v <vajay4834@gmail.com>
2023-11-21 09:03:54 +0530

    banner passing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import Cart from './pages/Cart';
-import LoginSignup from './pages/LoginSignup';
-import Product from './pages/Product';
-import Shop from './pages/Shop';
-import ShopCategory from './pages/ShopCategory';
-import Footer from './components/Footer/Footer';
-import men_banner from './components/Assets/banner_men.png';
-import women_banner from './components/Assets/banner_women.png';
-import kid_banner from './components/Assets/banner_kids.png';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Cart from "./pages/Cart";
+import LoginSignup from "./pages/LoginSignup";
+import Product from "./pages/Product";
+import Shop from "./pages/Shop";
+import ShopCategory from "./pages/ShopCategory";
+import Footer from "./components/Footer/Footer";
+import men_banner from "./components/Assets/banner_men.png";
+import women_banner from "./components/Assets/banner_women.png";
+import kid_banner from "./components/Assets/banner_kids.png";
 function App() {
   return (
     <>
       <Router>
         <NavBar />
         <Routes>
-          <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory  banner={men_banner} category="men" />} />
-          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
-          <Route path='/kids' element={<ShopCategory  banner={kid_banner} category="kid" />} />
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
           <Route path="/product" element={<Product />}>
-            <Route path=':productId' element={<Product />} />
+            <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/login' element={<LoginSignup />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </>
   );
