fe8b93bd7310c3606818daf21e878f782eb93fde
Ajay v <vajay4834@gmail.com>
2023-11-26 18:47:50 +0530

    detail product page added

--- frontend/src/components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import './ProductDisplay.css'
-const ProductDisplay = () => {
+import React from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+const ProductDisplay = ({ product }) => {
   return (
-    <div className='ProductDisplay'>
-        <div className="productleft">
-
+    <div className="ProductDisplay">
+      <div className="productleft">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
         </div>
-        <div className="productright">
-            
-        </div>
 
+        <div className="productdisplay-img">
+          <img src={product.image} alt="" className="productdisplay-main-img" />
+        </div>
+      </div>
+      <div className="productright">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-price">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">2:01</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
