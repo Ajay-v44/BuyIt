ee6a74aa3f64108c7d976544e9655550589191e4
Ajay v <vajay4834@gmail.com>
2023-11-27 20:07:01 +0530

    related product page created

---
+++ frontend/src/components/RelatedProducts/RelatedProduct.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./RelatedProduct.css";
+const RelatedProduct = () => {
+  return <div className="RelatedProduct">
+    <h1>Related Products</h1>
+    <hr />
+<div className="relatedproducts-item">
+    
+</div>
+
+  </div>;
+};
+
+export default RelatedProduct;
