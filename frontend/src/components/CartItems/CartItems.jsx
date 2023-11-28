93ffa86bda60e0fcde05b4999a308a0996be7257
Ajay v <vajay4834@gmail.com>
2023-11-28 11:11:12 +0530

    cart styled and completed

--- frontend/src/components/CartItems/CartItems.jsx
+++ frontend/src/components/CartItems/CartItems.jsx
@@ -10,16 +10,21 @@ const CartItems = () => {
   return (
     <div className="CartItems">
       <div className="cartitems-format-main">
+        
         <p>Products</p>
-        <p>Title</p> <p>Price</p> <p>Quantity</p> <p>Total</p>
+        <p>Title</p> 
+        <p>Price</p> 
+        <p>Quantity</p> 
+        <p>Total</p>
         <p>Remove</p>
+     
       </div>
       <hr />
       {all_product.map((e) => {
         if (cartitem[e.id] > 0) {
           return (
             <div key={e.id}>
-              <div className="cartitems-format">
+              <div className="cartitems-format cartitems-format-main">
                 <img src={e.image} alt="" className="carticon-product" />
                 <p>{e.name}</p>
                 <p>${e.new_price}</p>
@@ -34,11 +39,44 @@ const CartItems = () => {
                   }}
                 />
               </div>
+              <hr />
             </div>
           );
         }
-        return null; // Returning null for items where quantity is not greater than 0
+        return null; 
+        
       })}
+      <div className="cartitems-down">
+            <div className="cartitems-total">
+                <h1>cart Totals</h1>
+
+            </div>
+            <div className="cartitems-total-item">
+                <p>
+                   Subtotal 
+                </p>
+                <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-items">
+                <p>Shipping Fee</p>
+                <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-tottal-item">
+                <h3>
+                    Total
+                </h3>
+                <h3>${0}</h3>
+            </div>
+            <button>Proceed To Checkout</button>
+        </div>
+        <div className="cartitems-promocode">
+            <p>If you have  a promo code ,Enter it here</p>
+            <input type="text" placeholder="promo code" />
+            <button>Apply</button>
+
+        </div>
       {all_product.length > 0 && cartitem.filter((count) => count > 0).length === 0 && (
         <p>No items in cart</p>
       )}
